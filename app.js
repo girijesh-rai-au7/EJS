6805a2c34c3e0d9aa3c507407dc97d83cc314a07
pintu_24 <girijesh.pintu10@gmail.com>
2021-11-22 20:16:00 +0530

    used switch statement to change the value of each day

--- app.js
+++ app.js
@@ -11,7 +11,36 @@ app.set('view engine', 'ejs')
 app.get('/', (req,res)=>{
     let today = new Date();
     let currentDay = today.getDay();
-    let day = ""
+    let day = "";
+    switch (currentDay) {
+        case 0:
+            day = "sunday"
+            
+            break;
+            case 1:
+                day ="Monday";
+                break;
+                case 2:
+                    day = "Tuesday";
+                    break;
+                    case 3:
+                    day = "Wednesday";
+                    break;
+                    case 4:
+                    day = "Thursday";
+                    break;
+                    case 5:
+                    day = "Friday";
+                    break;
+                    case 6:
+                    day = "Saturday";
+                    break;
+                
+
+    
+        default:
+            break;
+    }
 
     if(currentDay=== 0 || currentDay === 6){
         day = "weekend"
